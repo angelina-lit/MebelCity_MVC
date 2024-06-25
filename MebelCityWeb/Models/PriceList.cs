346f1afe4e79f00387bf24bd1124db2ba9b55473
lit.lina <angelina.work96@gmail.com>
2024-06-25 22:58:45 +0500

    Добавила пакеты NuGet.

---
+++ MebelCityWeb/Models/PriceList.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MebelCityWeb.Models
+{
+	public class PriceList
+	{
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
