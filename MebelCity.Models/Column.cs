62404aefb197ee082b295224cdd53f3d5ea0149d
lit.lina <angelina.work96@gmail.com>
2024-06-27 16:23:58 +0500

    Частично реализовала View Edit, добавила модель PriceListDetailVM.

--- MebelCity.Models/Column.cs
+++ MebelCity.Models/Column.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel;
+using System.Web.Mvc;
 
 namespace MebelCity.Models
 {
@@ -12,8 +13,8 @@ namespace MebelCity.Models
 		[DisplayName("Наименование колонки")]
 		public string Name { get; set; }
 
-		[Required(ErrorMessage = "Это поле является обязательным к заполнению.")]
+		/*[Required(ErrorMessage = "Это поле является обязательным к заполнению.")]
 		[DisplayName("Тип содержимого колонки")]
-		public string TypeOfContent { get; set; }
+		public IEnumerable<SelectListItem> TypeOfContent { get; set; }*/
 	}
 }
---
+++ MebelCity.Models/ViewModel/PriceListDetailVM.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MebelCity.Models.ViewModel
+{
+	public class PriceListDetailVM
+	{
+		public int Id { get; set; }
+		[Required]
+		public PriceList PriceList { get; set; }
+
+		public Column Column { get; set; }
+	}
+}
