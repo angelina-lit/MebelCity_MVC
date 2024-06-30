95c34326e88b07bc5e076ff8c911b0272e509f1b
lit.lina <angelina.work96@gmail.com>
2024-06-30 14:45:57 +0500

    Внесла изменения в product.js.

---
+++ MebelCity.Models/PriceListDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MebelCity.Models
+{
+	public class PriceListDetails
+	{
+		public PriceList PriceList { get; set; }
+		public List<Product> Products { get; set; }
+	}
+}
--- MebelCityWeb/wwwroot/js/product.js
+++ MebelCityWeb/wwwroot/js/product.js
@@ -6,19 +6,18 @@ $(document).ready(function () {
 
 function loadDataTable() {
     dataTable = $('#tblData').DataTable({
-        "ajax": { url: '/admin/product/getall' },
+        "ajax": { url: '/product/getall' },
         "columns": [
-            { data: 'title', "width": "25%" },
-            { data: 'sku', "width": "15%" },
-            { data: 'price', "width": "10%" },
-            { data: 'producer', "width": "15%" },
-            { data: 'category.name', "width": "10%" },
+            { data: '', "width": "25%" },
+            { data: '', "width": "15%" },
+            { data: '', "width": "10%" },
+            { data: '', "width": "15%" },
+            { data: '', "width": "10%" },
             {
                 data: 'id',
                 "render": function (data) {
                     return `<div class="w-100 btn-group" role="group">
-                    <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2"> <i class ="bi bi-pencil-square"></i> Edit</a>
-                    <a onClick=Delete('/admin/product/delete/${data}') class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete</a>
+                    <a onClick=Delete('/product/delete/${data}') class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i>Удалить</a>
                     </div>`
                 },
                 "width": "25%"
@@ -29,13 +28,13 @@ function loadDataTable() {
 
 function Delete(url) {
     Swal.fire({
-        title: "Are you sure?",
+        title: "Вы уверены?",
         text: "You won't be able to revert this!",
         icon: "warning",
         showCancelButton: true,
         confirmButtonColor: "#3085d6",
         cancelButtonColor: "#d33",
-        confirmButtonText: "Yes, delete it!"
+        confirmButtonText: "Да, удалить!"
     }).then((result) => {
         if (result.isConfirmed) {
             $.ajax({
